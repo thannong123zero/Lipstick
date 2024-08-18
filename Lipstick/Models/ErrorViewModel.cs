92943d4361172298708be609902ee19b84d4ca0c
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-08-18 17:20:07 +0700

    Add project files.

---
+++ Lipstick/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lipstick.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
